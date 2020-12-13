af21b5d5d142c15f69ae21a3615a488f21cdbde2
Azhara <rimbaazhara55@gmail.com>
2020-12-13 12:41:47 +0700

    redis2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://https://safe-citadel-12386.herokuapp.com//cable'
-  config.action_cable.allowed_request_origins = [ 'http://safe-citadel-12386.herokuapp.com/', /http:\/\/safe-citadel-12386.herokuapp.com/.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://safe-citadel-12386.herokuapp.com/', /http:\/\/safe-citadel-12386.herokuapp.com/.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
