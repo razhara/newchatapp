37b305d68d2dd5ec76b3602af75b749713ff6a73
Azhara <rimbaazhara55@gmail.com>
2020-12-13 20:05:51 +0700

    migratenya

--- db/migrate/20201212134519_create_message_logs.rb
+++ db/migrate/20201212134519_create_message_logs.rb
@@ -3,6 +3,7 @@ class CreateMessageLogs < ActiveRecord::Migration[6.1]
     create_table :message_logs do |t|
       t.string :username
       t.text :body
+      t.string :jenis
 
       t.timestamps
     end
